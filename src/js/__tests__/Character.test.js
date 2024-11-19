732e031c49011f50a2697a2e85f4d99a12d22a8d
backsty <shalamovartem6@mail.ru>
2024-11-20 02:24:19 +0700

    fix: character tests

--- src/js/__tests__/Character.test.js
+++ src/js/__tests__/Character.test.js
@@ -1,16 +1,45 @@
 import { describe, test, expect } from '@jest/globals';
 import Character from '../Character.js';
-import Bowman from '../characters/Bowman.js';
 
 describe('Character', () => {
-    test('should throw error on new Character()', () => {
-        expect(() => new Character(1)).toThrow();
+    // Вспомогательный класс для тестирования
+    class TestCharacter extends Character {}
+
+    test('should throw error when instantiating base Character class', () => {
+        expect(() => new Character(1)).toThrow('Данный класс нельзя использовать для создания персонажа');
     });
 
-    test('should create instance of Bowman', () => {
-        const bowman = new Bowman(1);
-        expect(bowman).toBeInstanceOf(Character);
-        expect(bowman.attack).toBe(25);
-        expect(bowman.defence).toBe(25);
+    test('should create character when extending base class', () => {
+        const character = new TestCharacter(1);
+        expect(character).toBeDefined();
+    });
+
+    test('should set default values correctly', () => {
+        const character = new TestCharacter(2);
+        expect(character.level).toBe(2);
+        expect(character.attack).toBe(0);
+        expect(character.defence).toBe(0);
+        expect(character.health).toBe(50);
+        expect(character.type).toBe('generic');
+        expect(character.distance).toBe(1);
+        expect(character.distanceAttack).toBe(1);
+    });
+
+    test('should set custom type', () => {
+        const character = new TestCharacter(1, 'custom');
+        expect(character.type).toBe('custom');
+    });
+    
+    test('should maintain all properties when creating character', () => {
+        const character = new TestCharacter(4, 'test');
+        const props = Object.keys(character);
+        
+        expect(props).toContain('level');
+        expect(props).toContain('attack');
+        expect(props).toContain('defence');
+        expect(props).toContain('health');
+        expect(props).toContain('type');
+        expect(props).toContain('distance');
+        expect(props).toContain('distanceAttack');
     });
 });
