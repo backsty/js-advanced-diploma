69aa18cd45a0666cab263a684e037ac095d003f5
backsty <shalamovartem6@mail.ru>
2024-11-20 17:36:22 +0700

    feat: implemented start app tests

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -3,10 +3,70 @@ import GamePlay from '../GamePlay.js';
 import GameController from '../GameController.js';
 import GameStateService from '../GameStateService.js';
 
-jest.mock('./GamePlay');
-jest.mock('./GameController');
-jest.mock('./GameStateService');
+jest.mock('../GamePlay.js');
+jest.mock('../GameController.js');
+jest.mock('../GameStateService.js');
 
-describe('App inint', () => {
-    /// -
+describe('App initialization', () => {
+    let container;
+    
+    beforeEach(() => {
+        container = document.createElement('div');
+        container.id = 'game-container';
+        document.body.appendChild(container);
+
+        GamePlay.mockClear();
+        GameController.mockClear();
+        GameStateService.mockClear();
+    });
+
+    afterEach(() => {
+        document.body.innerHTML = '';
+        jest.resetAllMocks();
+    });
+
+    test('should initialize game components', async () => {
+        const mockBindToDOM = jest.fn();
+        const mockInit = jest.fn();
+        
+        GamePlay.prototype.bindToDOM = mockBindToDOM;
+        GameController.prototype.init = mockInit;
+        
+        // Запускаем код приложения
+        await import('../app.js');
+        
+        expect(GamePlay).toHaveBeenCalled();
+        expect(GameStateService).toHaveBeenCalledWith(localStorage);
+        expect(GameController).toHaveBeenCalled();
+        
+        expect(mockBindToDOM).toHaveBeenCalledWith(container);
+        expect(mockInit).toHaveBeenCalled();
+    });
+
+    test('should throw if game container not found',  async () => {
+        document.body.innerHTML = '';
+        
+        const initApp = async () => {
+            return new Promise(async (resolve, reject) => {
+                try {
+                    const app = await import('../app.js');
+                    
+                    await new Promise(resolveDOM => {
+                        document.dispatchEvent(new Event('DOMContentLoaded'));
+                        setTimeout(resolveDOM, 0);
+                    });
+                   
+                    const container = document.querySelector('#game-container');
+                    if (!container) {
+                        throw new Error('Game container not found');
+                    }
+                    resolve(app);
+                } catch (error) {
+                    reject(error);
+                }
+            });
+        };
+        
+        await expect(initApp()).rejects.toThrow('Game container not found');
+    });
 });
